5559dd5575649ea25ab4f3e72cac8253c8ada51d
Guilherme de Andrade <guiandrade52@yahoo.com.br>
2025-04-14 21:28:08 -0300

    feat: desenvolvido crud de usuário, tela home e contatos

---
+++ src/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using conectaOng.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace conectaOng.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options): base(options)
+        {
+            
+        }
+
+        public DbSet<User> User { get; set; }
+    }
+}
